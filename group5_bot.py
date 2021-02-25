46225f7982fcb5e622ecf9fa54ee76571bdc7ebc
Greg Filippov <lookupninja@gmail.com>
2021-02-24 23:25:25 -0500

    12

--- group5_bot.py
+++ group5_bot.py
@@ -14,37 +14,44 @@ app = Flask(__name__)
 
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call('auth.test')['user_id']
 
-BOT_ID =  client.api_call('auth.test')['user_id']
-
+# return boot message when root of the domain as been accessed
 @app.route('/')
-def hello():
+def wakeup():
     return "Dyno Up and Running... :)"
-    
+
+# extract and process user message    
 @slack_event_adapter.on('message')
 def message(payload):
-    #print(payload)
+    channel_id = event.get('channel')    
     event = payload.get('event',{})
-    channel_id = event.get('channel')
     user_id = event.get('user')
-    text2 = event.get('text')
+    input_text = event.get('text')
 
     if BOT_ID != user_id:
-        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + text2 + '&units=metric&appid=' + os.environ['WEATHER_KEY']
+        # build API url
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + input_text + '&units=metric&appid=' + os.environ['WEATHER_KEY']
         response = requests.get(url)   
 
+        # check if valid city name have been entered
         if response.status_code == requests.codes.ok:
+
+            # get weather api response
             weather_response = response.text
             weather_data = json.loads(weather_response)["main"]
-            
             #print (weather_data)
             
-            output = text2.capitalize() + " current weather: \n"
+            output = input_text.capitalize() + " current weather: \n"
             output += "\tTemp: " + str(weather_data['temp']) + " C\n"
-            output += "\tFeels Like: " + str(weather_data['feels_like']) + " C"
- 
+            output += "\tFeels Like: " + str(weather_data['feels_like']) + " C\n"
+            output += "\tPressure: " + str(weather_data['pressure']) + " hPa\n"
+            output += "\tHumidity: " + str(weather_data['humidity']) + " %"
             #print( output )    
             
+            # print weather details to the user
             client.chat_postMessage(channel=channel_id, text=output)
+        
         else:
-            client.chat_postMessage(channel=channel_id, text=text2)
+            # print back user message
+            client.chat_postMessage(channel=channel_id, text=input_text)
